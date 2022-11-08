bcb767b6cfd5d46b940ee395ef3c08888289c11a
Mayas Faraj <uniqueprogrammer@hotmail.com>
2022-11-09 02:57:47 +0300

    add express module

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const http=require("http");
+const express=require("exprees");
 const server=http.createServer((req, res)=>res.end("hello world"));
 server.listen(3000, "localhost", ()=>console.log("started.."));
+
+//use express
+const app=express();
+app.get("/", (req, res)=>res.end("hello world"));
+app.listen(3000, ()=>console.log("started2.."));
